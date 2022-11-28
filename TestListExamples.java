0e11daea23558f1926413972241c288aa96d917f
ceciliatruong <114256675+ceciliatruong@users.noreply.github.com>
2022-11-28 03:16:22 -0800

    commit

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,34 @@
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testMerge() {
+      List<String> list1 = Arrays.asList("ab", "donut", "orange");
+      List<String> list2 = Arrays.asList("a", "donut", "ornge");
+      assertEquals(Arrays.asList("a", "ab", "donut", "donut", "orange", "ornge"), ListExamples.merge(list1, list2));
+  }
+  @Test
+  public void testFilter() {
+      StringChecker newChecker = new StringChecker() {
+          @Override
+          public boolean checkString(String s) {
+              if(s.length() > 1) {
+                  return true;
+              }
+              else {
+                  return false;
+              }
+          }
+      };
+      List<String> list1 = Arrays.asList("donut", "orange");
+      List<String> list2 = Arrays.asList("a", "donut", "orange");
+      list2 = ListExamples.filter(list2, newChecker);
+      assertEquals(list1, list2);
+  }
 }
--- grade.sh
+++ grade.sh
@@ -4,4 +4,37 @@ set -e
 
 rm -rf student-submission
 git clone $1 student-submission
-
+if [[ -f ./student-submission/ListExamples.java ]]; then
+    echo "ListExamples was found in the repository."
+else
+    echo "ListExamples was not found in the repository."
+    exit
+fi 
+cp TestListExamples.java ./student-submission
+cp -r ./lib ./student-submission/lib
+cd student-submission
+set +e
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java 2> compile-err.txt
+java -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > compile-err.txt
+grep -B 1 "Error:" compile-err.txt > tests-success.txt
+testMerge=$( grep -c "testMerge" tests-success.txt )
+testFilter=$( grep -c "testFilter" tests-success.txt )
+testTotal=$(($testMerge + $testFilter))
+if [[ $testTotal -eq 0 ]]; then
+    echo "Passed all tests! Score: 2/2"
+    exit
+fi
+if [[ $testMerge -eq 1 ]]; then
+    echo "Failed testMerge."
+fi
+if [[ $testFilter -eq 1 ]]; then
+    echo "Failed testFilter."
+fi
+if [[ $testTotal -eq 1 ]]; then
+    echo "Score: 1/2"
+    exit
+fi
+if [[ $testTotal -eq 2 ]]; then
+    echo "Score: 0/2"
+    exit
+fi
